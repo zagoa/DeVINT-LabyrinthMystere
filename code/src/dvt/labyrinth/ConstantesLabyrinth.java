445e5b9e868c6c2faa2916adeff2f6b921f2c80d
Lordsionn <etienne.laskar74@gmail.com>
2016-03-01 13:01:56 +0100

    Ajout constantes de path

--- code/src/dvt/labyrinth/ConstantesLabyrinth.java
+++ code/src/dvt/labyrinth/ConstantesLabyrinth.java
@@ -10,6 +10,8 @@ public class ConstantesLabyrinth {
     public static final String TWOPLAYERS = "2 joueurs";
     public static final String HELP = "Aide";
     public static final String QUIT = "Quitter";
+    public static final String IMAGEPATH = "../ressources/images/";
+    public static final String SOUNDPATH = "../ressources/sons/";
 
     public static final int MARGE_LEFT_RIGHT = 80;
     public static final int MARGE_TOP_BOT = 3;
