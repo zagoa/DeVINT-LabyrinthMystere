cbeef2ab13e03f065ebb68bfb3345c43fd53063d
Thomas DE LA RED <thomas.delared@laposte.net>
2016-03-10 21:24:36 +0100

    [CREATE] Game class [UPDATE] A pawn has no name, it's the player who has a name

---
+++ code/src/dvt/labyrinth/model/Game1vs1.java
@@ -0,0 +1,25 @@
+package dvt.labyrinth.model;
+
+import dvt.labyrinth.Tray;
+
+/**
+ * Created by user on 10/03/2016.
+ */
+public class Game1vs1 {
+    private Tray tray;
+    private Player player1;
+    private Player player2;
+
+    public Game1vs1(Tray tray, Player player1, Player player2){
+        this.tray = tray;
+        this.player1 = player1;
+        this.player2 = player2;
+
+    }
+
+
+    public void startGame(){
+
+    }
+
+}
