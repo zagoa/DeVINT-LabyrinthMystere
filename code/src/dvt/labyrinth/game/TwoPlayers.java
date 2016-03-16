e53fd5e2e6a74ec17169ee8f46bce77d5373e24b
Adrian PALUMBO <pa506143@etu.unice.fr>
2016-03-16 18:20:00 +0100

    [Add] Winner sound text

--- code/src/dvt/labyrinth/game/TwoPlayers.java
+++ code/src/dvt/labyrinth/game/TwoPlayers.java
@@ -14,6 +14,7 @@ import java.awt.*;
 import java.awt.event.*;
 
 import static dvt.labyrinth.ConstantesLabyrinth.*;
+import static dvt.devint.ConstantesDevint.*;
 
 /**
  * The Labyrinth class.
@@ -241,6 +242,4 @@ public class TwoPlayers extends Jeu {
     public boolean isSettingWall() {
         return settingWall;
     }
-
-
 }
