bc1ef0a1d58f97cb08cf98a578e1107108fcecf6
Adrian PALUMBO <pa506143@etu.unice.fr>
2016-05-10 14:54:18 +0200

    [Add] Livraison: src/labyrinth

---
+++ livraison/src/dvt/labyrinth/model/Bonus.java
@@ -0,0 +1,16 @@
+package dvt.labyrinth.model;
+
+import dvt.labyrinth.model.essential.Item;
+import dvt.labyrinth.tools.ConstantesLabyrinth;
+
+/**
+ * A bonus item
+ *
+ * @author Etienne
+ */
+public class Bonus extends Item {
+    //TODO Not needed for MVC
+    public Bonus(ConstantesLabyrinth.RESOURCES res) {
+        super(res);
+    }
+}
---
+++ livraison/src/dvt/labyrinth/tools/Position.java
@@ -0,0 +1,63 @@
+package dvt.labyrinth.tools;
+
+/**
+ * This class is used to keep x & y coordinates.
+ * We use them with Players
+ *
+ * @author Adrian & Thomas
+ */
+public class Position {
+    private int x;
+    private int y;
+
+    /**
+     * Constructor
+     *
+     * @param x
+     *          The x coordinate
+     * @param y
+     *          The y coordinate
+     */
+    public Position(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Getter on x
+     * @return x
+     */
+    public int getX() {
+        return x;
+    }
+
+    /**
+     * Getter on y
+     * @return y
+     */
+    public int getY() {
+        return y;
+    }
+
+    /**
+     * Setter on x
+     * @param x
+     *          The new x value
+     */
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    /**
+     * Setter on y
+     * @param y
+     *          The new y value
+     */public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return "("+x+","+y+")";
+    }
+}
